f9d5177f30b8cc058e93915fd149c888a81e73f3
DanielJC06 <danieljeancox@gmail.com>
2021-02-16 01:16:28 -0600

    munge key env

--- slurm_ctl/start_services.sh
+++ slurm_ctl/start_services.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+cp /etc/munge/munge.key.tmp/MUNGE_KEY /etc/munge/munge.key
+chown munge:munge /etc/munge/munge.key
+chmod 0700 /etc/munge/munge.key
 service ssh start
 service munge start
 service slurmctld start
--- slurm_worker/start_ssh_server.sh
+++ slurm_worker/start_ssh_server.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#! /bin/sh
 
+cp /etc/munge/munge.key.tmp/MUNGE_KEY /etc/munge/munge.key
+chown munge:munge /etc/munge/munge.key
+chmod 0700 /etc/munge/munge.key
 service ssh start
 service munge start
-service slurmd start
+service slurmctld start
 sleep infinity
